e8c3c9e44b280761a4a0075d9451f2129299e3d5
Egor Kuznechyk <nayone.tapok@gmail.com>
2025-10-03 23:24:14 +0300

    Allow all formats for cloudinary

--- dev/utils/cloudinary.ts
+++ dev/utils/cloudinary.ts
@@ -12,8 +12,7 @@ export const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'img',
-    format: 'auto',
-    allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp']
+    format: 'auto'
   },
 })
 
